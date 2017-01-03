33f9314295d8c59b49e97e16b216a03c17f1dbd9
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-01-03 16:12:27 +0100

    Added die and dicecup to the game

--- 33_CDIO4/src/entity/DiceCup.java
+++ 33_CDIO4/src/entity/DiceCup.java
@@ -1,5 +1,39 @@
 package entity;
 
-public class DiceCup {
+import entity.Die;
 
+public class DiceCup 
+{	
+	//Instance variables
+	private Die d1;
+	private Die d2;
+	
+	/**
+	 * Object DiceCup constructor. 
+	 * The diceCup object contains two die Objects.
+	 */
+	public DiceCup()
+	{
+		d1 = new Die();
+		d2 = new Die();
+	}
+	
+	/**
+	 * Method rollDice rolls both dice in the DiceCup object.
+	 */
+	public void shakeCup()
+	{
+		d1.rollDie();
+		d2.rollDie();
+	}
+
+	/**
+	 * Method getDiceValue returns the value of the rolled dice as a integer array.
+	 * @return Returns the array with the two dice values.
+	 */
+	public int[] getDiceValue()
+	{
+		int[] array = {d1.getValue(), d2.getValue()};
+		return array;
+	}	
 }

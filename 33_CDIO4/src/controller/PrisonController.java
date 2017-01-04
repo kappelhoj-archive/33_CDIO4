7c513193d90e834baea6ee250cbb9744b01ec265
Jeppe <Jeppe@DESKTOP-HUK674P.eduroam.wireless.dtu.dk>
2017-01-04 08:55:38 +0100

    Updated PrisonController

--- 33_CDIO4/src/controller/PrisonController.java
+++ 33_CDIO4/src/controller/PrisonController.java
@@ -1,16 +1,32 @@
 package controller;
 
+import desktop_resources.GUI;
 import entity.Player;
 
 public class PrisonController {
+	
+	private MainController mainController;
 
 	public void sentToPrison(Player player)
 	{
 		player.setPrison(true);
+		player.setPosition(31);
+		mainController.movePlayerOnGUI();
 	}
 	
-	public void inPrison()
+	public boolean inPrison()
 	{
+		GUI.getUserSelection("Du er i fængsel, hvad vil du gøre?", "Betal dig ud: 1.000,-", "Rul med terningerne");
 		
+		
+		mainController.rollDice();
+		if(mainController.checkForExtraTurn())
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 	}
 }

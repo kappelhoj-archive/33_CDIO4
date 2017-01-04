0ed47e205018bbf5901bb6783a760c77fc0a54a1
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-01-04 09:10:43 +0100

    Added a filereader test

--- 33_CDIO4/src/test/RunningReaderTest.java
+++ 33_CDIO4/src/test/RunningReaderTest.java
@@ -1,5 +1,13 @@
 package test;
 
-public class RunningReaderTest {
+import controller.Reader;
 
+public class RunningReaderTest {
+	
+	public static void main(String[] args) {
+		Reader reader = new Reader();
+		reader.openFile("../data/Feltliste.dat");
+		
+		
+	}
 }

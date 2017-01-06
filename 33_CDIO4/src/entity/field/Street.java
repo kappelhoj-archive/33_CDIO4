61fc81097a70b57db480b6f0ffe6af063eff9c0e
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-01-06 13:46:15 +0100

    Added double rent to streets

--- 33_CDIO4/src/entity/field/Street.java
+++ 33_CDIO4/src/entity/field/Street.java
@@ -1,8 +1,7 @@
 package entity.field;
 
-public class Street extends Ownable
-{
-	//Instance variables
+public class Street extends Ownable {
+	// Instance variables
 	private String colour;
 	private int baseRent;
 	private int housePrice;
@@ -10,22 +9,31 @@ public class Street extends Ownable
 	private int numbOfHouses;
 	private int pledge;
 
-
 	/**
 	 * constructor: Constructs a Street.
-	 * @param name The name of the field.
-	 * @param type The type of the field.
-	 * @param description The description of the field.
-	 * @param price The price of the field.
-	 * @param colour The specific colour of the field on the board.
-	 * @param baseRent The base rent of the unbuilt field.
-	 * @param houseRent The base rent of the empty field.
-	 * @param numbOfHouses The current number of houses built on the field.
-	 * @param pledge The value of a pledged field.
+	 * 
+	 * @param name
+	 *            The name of the field.
+	 * @param type
+	 *            The type of the field.
+	 * @param description
+	 *            The description of the field.
+	 * @param price
+	 *            The price of the field.
+	 * @param colour
+	 *            The specific colour of the field on the board.
+	 * @param baseRent
+	 *            The base rent of the unbuilt field.
+	 * @param houseRent
+	 *            The base rent of the empty field.
+	 * @param numbOfHouses
+	 *            The current number of houses built on the field.
+	 * @param pledge
+	 *            The value of a pledged field.
 	 */
 
-	public Street (String name, String type, String description, int price, String colour, int baseRent, int housePrice, int[] houseRent, int pledge)
-	{
+	public Street(String name, String type, String description, int price, String colour, int baseRent, int housePrice,
+			int[] houseRent, int pledge) {
 		super(name, type, description, price);
 		this.colour = colour;
 		this.baseRent = baseRent;
@@ -37,41 +45,53 @@ public class Street extends Ownable
 
 	/**
 	 * Method getColour: Returns the colour of the field.
+	 * 
 	 * @return The colour of the field.
 	 */
-	public String getColour(){
+	public String getColour() {
 		return colour;
 	}
 
 	/**
 	 * Method getBaseRent: Returns the rent to be paid.
 	 */
-	public int getBaseRent(){
+	public int getBaseRent() {
 		return baseRent;
 	}
 
 	/**
-	 * Method getHouseRent: Returns the rent to be paid by a player landing on a built field.
+	 * Method getHouseRent: Returns the rent to be paid by a player landing on a
+	 * built field.
 	 */
-	public int getHouseRent(int numbOfHouses){
+	public int getHouseRent(int numbOfHouses) {
 		return houseRent[numbOfHouses];
 	}
+
 	/**
-	 * Method getRent: Calculates and returns the rent to be paid, depending on the number of houses.
+	 * Method getRent: Calculates and returns the rent to be paid, depending on
+	 * the number of houses.
 	 */
-	public int getRent(){
+	public int getRent() {
 		int rent = 0;
 
-		if (numbOfHouses == 0)
-		{
+		if (numbOfHouses == 0) {
 			rent = baseRent;
-			switch(super.getOwner().getStreetsOwned(colour)){
 			
+			int streetsNeeded;
+			switch (colour) {
+			case "Blå":
+			case "Lilla":
+				streetsNeeded=2;
+				break;
+			default:
+				streetsNeeded=3;
+				break;
 			}
-			
-		}
-		else
-		{
+			if(super.getOwner().getStreetsOwned(colour)==streetsNeeded){
+				rent=rent*2;
+			}
+
+		} else {
 			rent = houseRent[numbOfHouses];
 		}
 
@@ -79,51 +99,47 @@ public class Street extends Ownable
 	}
 
 	/**
-	 * Method getPledge: Returns the value of the pledge given to the player, by the bank, when the player pledges the field.
+	 * Method getPledge: Returns the value of the pledge given to the player, by
+	 * the bank, when the player pledges the field.
 	 */
-	public int getPledge(){
+	public int getPledge() {
 		return pledge;
 	}
 
 	/**
-	 * Method getValue: Returns the value of field (field price + prices for houses). <br>
+	 * Method getValue: Returns the value of field (field price + prices for
+	 * houses). <br>
+	 * 
 	 * @return The value of the field.
 	 */
 	@Override
-	public int getValue()
-	{
+	public int getValue() {
 		return super.getValue() + numbOfHouses * housePrice;
 	}
 
-	public int getHousePrice()
-	{
+	public int getHousePrice() {
 		return housePrice;
 	}
-
 
 	/**
 	 * Methods needed for the Bank class. Might need revisiting later.
 	 */
-	public int getNumbOfHouses()
-	{
+	public int getNumbOfHouses() {
 		return numbOfHouses;
 	}
-	public int subtractNumbOfHouses()
-	{
-		if (numbOfHouses == 0)
-		{
+
+	public int subtractNumbOfHouses() {
+		if (numbOfHouses == 0) {
 			System.out.println("Du har ikke nogen huse på " + getName() + ".");
 		}
-		if (numbOfHouses > 0)
-		{
+		if (numbOfHouses > 0) {
 			numbOfHouses--;
 			System.out.println("Ét hus er blevet fjernet fra " + getName() + ".");
 		}
 		return numbOfHouses;
 	}
-	
-	public void setNumbOfHouses(int houseLoss)
-	{
+
+	public void setNumbOfHouses(int houseLoss) {
 		numbOfHouses = houseLoss;
 	}
 }

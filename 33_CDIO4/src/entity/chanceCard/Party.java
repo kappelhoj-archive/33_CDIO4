fa7c83fb001ce7d8df478c5b1546e12a2085f472
Mikkel <Mikkel@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2017-01-10 13:23:38 +0100

    Comment
    - To Party class.
    - To Payment class.
    - To Prison class.
    - To TaxCard class.

--- 33_CDIO4/src/entity/chanceCard/Party.java
+++ 33_CDIO4/src/entity/chanceCard/Party.java
@@ -5,6 +5,12 @@ public class Party extends ChanceCard{
 	//Instance variables
 	private int cost;
 	
+	/**
+	 * Constructor: Constructs a Party ChanceCard.
+	 * @param type The type of the ChanceCard
+	 * @param description The description of the ChanceCard
+	 * @param cost The cost of the party.
+	 */
 	public Party(String type, String description,int cost)
 	{
 		super(type,description);

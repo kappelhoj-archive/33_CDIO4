9c726a81b221948ad563a201dbec2508607afa7c
Mikkel <Mikkel@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2017-01-10 12:08:33 +0100

    Added comments to MoveToField.

--- 33_CDIO4/src/entity/chanceCard/MoveToField.java
+++ 33_CDIO4/src/entity/chanceCard/MoveToField.java
@@ -5,6 +5,12 @@ public class MoveToField extends Movement {
 	//Instance variables
 	private int fieldPosition;
 	
+	/**
+	 * Constructor: Constructs a MoveToField Movement ChanceCard.
+	 * @param type The type of the Movement ChanceCard.
+	 * @param description The description of the Movement ChanceCard.
+	 * @param fieldPosition The position of the field to move to.
+	 */
 	public MoveToField(String type, String description, int fieldPosition)
 	{
 		super(type, description);

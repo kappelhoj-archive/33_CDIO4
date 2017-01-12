4476f73c5127bf99391f07450d201605e2687b78
Mads <mads@stege.eu>
2017-01-12 11:02:58 +0100

    Corrected errors in the brewery test. See subnotes.
    The JUnit tests to see if:
    - The rent calculated for a single owner with 1 brewery.
    - The rent calculated for a single owner with 2 breweries.

--- 33_CDIO4/src/test/BreweryTest.java
+++ 33_CDIO4/src/test/BreweryTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.After;
-import entity.field.*;
+import entity.field.Brewery;
 import entity.Player;
-import entity.field.*;
-
 
 import org.junit.Test;
 
@@ -16,37 +14,77 @@ public class BreweryTest {
 	Player player;
 	Player player2;
 	Brewery tuborg;
-	
-	
-	
+	Brewery cola;
+
 	@Before
-	public void setUp() throws Exception{
-		player = new Player ("Mikkel");
-		player2 = new Player ("Simon");
-		tuborg = new Brewery (13,"Tuborg","Brewery","Bryggeri",3000);
+	public void setUp() throws Exception {
+		player = new Player("Mikkel");
+		player2 = new Player("Simon");
+		tuborg = new Brewery(13, "Tuborg", "Tapperi", "Bryggeri", 3000);
+		cola = new Brewery(29, "Cocal Cola", "Tapperi", "Bryggeri", 3000);
+
 	}
-	
-	
+
 	@After
 	public void tearDown() throws Exception {
 		tuborg = null;
 	}
-	
+
+	/**
+	 * Test-ID: XX XX The method test_singleBreweryRent tests the credibility of
+	 * the rent calculation when a brewery is asked to calculate the rent when
+	 * the diceSum is 10, and only a single brewery is owned.
+	 */
 	@Test
-	
-	public void test() {
-		tuborg.setOwner(player2);
-		int baseRent = 100;
-		int diceSum = 10;
-		int numbOfBreweries = 1;
-		tuborg.setDiceSum(diceSum);
-		player.payRent(player2, tuborg.getRent());
-		
-		int expected = 29000;
-		int actual = player.getAccountBalance();
-		System.out.println(player.getAccountBalance());
+	public void test_singleBreweryRent() {
+		tuborg.setOwner(player);
+		player.setFields(tuborg);
+		tuborg.setDiceSum(10);
+		int tuborgRent = tuborg.getRent();
+
+		int expected = 1000;
+		int actual = tuborgRent;
 		assertEquals(expected, actual);
-		
+	}
+
+	/**
+	 * Test-ID: XX XX The method test_multipleBreweriesRent tests the
+	 * credibility of the rent calculation when a brewery is asked to calculate
+	 * the rent when the diceSum is 10, and more than one brewery is owned by
+	 * the same player.
+	 */
+	@Test
+	public void test_multipleBreweriesRent() {
+		tuborg.setOwner(player);
+		cola.setOwner(player);
+		player.setFields(tuborg);
+		player.setFields(cola);
+		tuborg.setDiceSum(10);
+		int tuborgRent = tuborg.getRent();
+
+		int expected = 2000;
+		int actual = tuborgRent;
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Test-ID: XX XX The method test_multipleBreweriesRent tests the
+	 * credibility of the rent calculation when a brewery is asked to calculate
+	 * the rent when the diceSum is 10, and each brewery is owned by two
+	 * different players.
+	 */
+	@Test
+	public void test_multipleBreweriesRentDifferentOwners() {
+		tuborg.setOwner(player);
+		cola.setOwner(player2);
+		player.setFields(tuborg);
+		player2.setFields(cola);
+		tuborg.setDiceSum(10);
+		int tuborgRent = tuborg.getRent();
+
+		int expected = 1000;
+		int actual = tuborgRent;
+		assertEquals(expected, actual);
 	}
 
 }
